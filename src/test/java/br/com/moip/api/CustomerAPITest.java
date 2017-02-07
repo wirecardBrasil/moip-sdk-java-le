fb79b966e9a6ea4a8aee7c6090a5414f5257ff01
Joao Santana <joaosantana.ti@gmail.com>
2017-02-07 15:45:38 -0200

    Removendo construcao de modelo de request -- nao é necessário nesse tipo de teste

--- src/test/java/br/com/moip/api/CustomerAPITest.java
+++ src/test/java/br/com/moip/api/CustomerAPITest.java
@@ -1,22 +1,12 @@
 package br.com.moip.api;
 
-import br.com.moip.request.ApiDateRequest;
-import br.com.moip.request.CreditCardRequest;
 import br.com.moip.request.CustomerRequest;
-import br.com.moip.request.FundingInstrumentRequest;
-import br.com.moip.request.HolderRequest;
-import br.com.moip.request.PhoneRequest;
-import br.com.moip.request.ShippingAddressRequest;
-import br.com.moip.request.TaxDocumentRequest;
 import br.com.moip.resource.Customer;
 import com.rodrigosaito.mockwebserver.player.Play;
 import com.rodrigosaito.mockwebserver.player.Player;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -26,34 +16,17 @@ public class CustomerAPITest {
     @Rule
     public Player player = new Player();
     private CustomerAPI api;
+    private CustomerRequest customerRequest;
 
     @Before
     public void setUp() {
+        customerRequest = new CustomerRequest();
         api = new CustomerAPI(new ClientFactory().client(player.getURL("").toString()));
     }
 
     @Play("customer/create")
     @Test
     public void createCustomer() {
-        CustomerRequest customerRequest = new CustomerRequest()
-                .ownId("own_id")
-                .email("jose_silva0@email.com")
-                .birthdate(new ApiDateRequest()
-                        .date(new GregorianCalendar(1988, Calendar.DECEMBER, 30)
-                                .getTime()))
-                .taxDocument(TaxDocumentRequest.cpf("22222222222"))
-                .phone(new PhoneRequest()
-                        .setNumber("66778899")
-                        .setAreaCode("11"))
-                .shippingAddressRequest(new ShippingAddressRequest()
-                        .city("Sao Paulo")
-                        .complement("8")
-                        .district("Itaim")
-                        .state("SP")
-                        .streetNumber("2927")
-                        .zipCode("01234000")
-                        .country("BRA"));
-
         Customer customer = api.create(customerRequest);
 
         assertEquals("CUS-Y6L4AGQN8HKQ", customer.getId());
@@ -92,13 +65,6 @@ public class CustomerAPITest {
     @Play("customer/create_minimal_customer")
     @Test
     public void createMinimalCustomer() {
-        CustomerRequest customerRequest = new CustomerRequest()
-                .ownId("own_id")
-                .email("jose_silva0@email.com")
-                .birthdate(new ApiDateRequest()
-                        .date(new GregorianCalendar(1988, Calendar.DECEMBER, 30)
-                                .getTime()));
-
         Customer customer = api.create(customerRequest);
 
         assertEquals("CUS-YZA5DJ41GFXU", customer.getId());
@@ -112,35 +78,6 @@ public class CustomerAPITest {
     @Play("customer/create_customer_with_funding_instrument")
     @Test
     public void createCustomerWithFundingInstrument() {
-        CustomerRequest customerRequest = new CustomerRequest()
-                .ownId("own_id")
-                .email("jose_silva0@email.com")
-                .birthdate(new ApiDateRequest()
-                        .date(new GregorianCalendar(1988, Calendar.DECEMBER, 30)
-                                .getTime()))
-                .taxDocument(TaxDocumentRequest.cpf("22222222222"))
-                .phone(new PhoneRequest()
-                        .setNumber("66778899")
-                        .setAreaCode("11"))
-                .shippingAddressRequest(new ShippingAddressRequest()
-                        .city("Sao Paulo")
-                        .complement("8")
-                        .district("Itaim")
-                        .state("SP")
-                        .streetNumber("2927")
-                        .zipCode("01234000")
-                        .country("BRA"))
-                .fundingInstrument(new FundingInstrumentRequest()
-                        .creditCard(new CreditCardRequest()
-                                .holder(new HolderRequest()
-                                        .birthdate("1988-12-30")
-                                        .fullname("Jose Portador")
-                                        .phone(new PhoneRequest()
-                                                        .setAreaCode("11")
-                                                        .setNumber("11111111")
-                                                        .countryCode("55")
-                                        ))));
-
         Customer customer = api.create(customerRequest);
 
         assertEquals("CUS-O7FVGTW6DBJI", customer.getId());
