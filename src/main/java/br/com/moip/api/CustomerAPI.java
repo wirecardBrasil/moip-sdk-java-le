8c0ea42f59f2fd226047d6d3ae4807f13ff5e046
Caue Ferreira <eldender@gmail.com>
2017-03-14 10:54:20 -0300

    Fixed addFundingInstrument method

--- src/main/java/br/com/moip/api/CustomerAPI.java
+++ src/main/java/br/com/moip/api/CustomerAPI.java
@@ -3,6 +3,7 @@ package br.com.moip.api;
 import br.com.moip.Client;
 import br.com.moip.request.CustomerRequest;
 import br.com.moip.resource.Customer;
+import br.com.moip.resource.FundingInstrument;
 
 public class CustomerAPI {
 
@@ -16,8 +17,8 @@ public class CustomerAPI {
         return client.post("/v2/customers", customer, Customer.class);
     }
 
-    public Customer addFundingIntrument(CustomerRequest customer) {
-        return client.post("/v2/customers/" + customer.getId() + "/fundinginstruments", customer.getFundingInstruments(), Customer.class);
+    public FundingInstrument addFundingIntrument(CustomerRequest customer) {
+        return client.post("/v2/customers/" + customer.getId() + "/fundinginstruments", customer.getFundingInstrument(), FundingInstrument.class);
     }
 
     public Customer get(String externalId) {
