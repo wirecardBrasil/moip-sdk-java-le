99ce1faa0c79e5d853f49d317f17612fb31da86d
Caio Gaspar <caio.gaspar@moip.com.br>
2017-07-27 16:56:10 -0300

    Adding new properties to installment and Receiver objects

--- src/main/java/br/com/moip/resource/Installment.java
+++ src/main/java/br/com/moip/resource/Installment.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Installment {
 
     private int[] quantity;
+    private int addition;
+    private int discount;
 
     public int[] getQuantity() {
         return quantity;
@@ -12,13 +14,25 @@ public class Installment {
 
     @Override
     public String toString() {
-        return "Installment{" +
-                "quantity=" + Arrays.toString(quantity) +
-                '}';
+        return new StringBuilder("Installment{")
+                .append("quantity='").append(Arrays.toString(quantity))
+                .append(", addition='").append(addition)
+                .append(", discount='").append(discount)
+                .append('}').toString();
     }
 
     public Installment setQuantity(int[] quantity) {
         this.quantity = quantity;
+        return this;
+    }
+
+    public Installment setAddition(int addition) {
+        this.addition = addition;
+        return this;
+    }
+
+    public Installment setDiscount(int discount) {
+        this.discount = discount;
         return this;
     }
 }
