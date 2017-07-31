f27421dd6d814ff2f86a964e63e1457577a25cb2
Caio Gaspar <caio.gaspar@moip.com.br>
2017-07-31 10:26:49 -0300

    Fixing imports

--- src/test/java/br/com/moip/api/OrderAPITest.java
+++ src/test/java/br/com/moip/api/OrderAPITest.java
@@ -22,7 +22,10 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.UUID;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class OrderAPITest {
 
