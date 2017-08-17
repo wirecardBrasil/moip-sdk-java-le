7f8739a5157bb350b85d8999613825d890aaf99e
Ralph Avalon <amoedo.raphael@gmail.com>
2017-08-16 21:54:25 -0300

    Fixing indentation

--- src/main/java/br/com/moip/request/OrderRequest.java
+++ src/main/java/br/com/moip/request/OrderRequest.java
@@ -5,109 +5,107 @@ import java.util.List;
 
 public class OrderRequest {
 
-    private String ownId;
-    private OrderAmountRequest amount;
-    private final List<ItemRequest> items = new ArrayList<ItemRequest>();
-    private CustomerRequest customer;
-    private final List<ReceiverRequest> receivers = new ArrayList<ReceiverRequest>();
+	private String ownId;
+	private OrderAmountRequest amount;
+	private final List<ItemRequest> items = new ArrayList<ItemRequest>();
+	private CustomerRequest customer;
+	private final List<ReceiverRequest> receivers = new ArrayList<ReceiverRequest>();
 
-    public String getOwnId() {
-        return ownId;
-    }
-
-    public OrderRequest ownId(final String ownId) {
-        this.ownId = ownId;
-
-        return this;
-    }
-    
-    public OrderAmountRequest getAmount() {
-		return amount;
+	public String getOwnId() {
+		return ownId;
 	}
-    
-    public OrderRequest amount(OrderAmountRequest amount) {
-    	this.amount = amount;
-    	
+
+	public OrderRequest ownId(final String ownId) {
+		this.ownId = ownId;
+
 		return this;
 	}
 
-    public List<ItemRequest> getItems() {
-        return items;
-    }
+	public OrderAmountRequest getAmount() {
+		return amount;
+	}
 
-    public OrderRequest addItem(final String product, final Integer quantity, final String detail, final Integer price) {
-        items.add(new ItemRequest(product, quantity, detail, price));
+	public OrderRequest amount(OrderAmountRequest amount) {
+		this.amount = amount;
 
-        return this;
-    }
+		return this;
+	}
 
-    public List<ReceiverRequest> getReceivers() {
-        return receivers;
-    }
+	public List<ItemRequest> getItems() {
+		return items;
+	}
 
-    public OrderRequest addReceiver(final ReceiverRequest receiverRequest) {
-        receivers.add(receiverRequest);
+	public OrderRequest addItem(final String product, final Integer quantity, final String detail,
+			final Integer price) {
+		items.add(new ItemRequest(product, quantity, detail, price));
 
-        return this;
-    }
+		return this;
+	}
 
-    public CustomerRequest getCustomer() {
-        return customer;
-    }
+	public List<ReceiverRequest> getReceivers() {
+		return receivers;
+	}
 
-    public OrderRequest customer(final CustomerRequest customer) {
-        this.customer = customer;
+	public OrderRequest addReceiver(final ReceiverRequest receiverRequest) {
+		receivers.add(receiverRequest);
 
-        return this;
-    }
+		return this;
+	}
 
-    @Override
-    public String toString() {
-        return new StringBuilder("OrderRequest{")
-                .append("ownId='").append(ownId).append('\'')
-                .append(", items=").append(items)
-                .append(", customer=").append(customer)
-                .append('}').toString();
-    }
+	public CustomerRequest getCustomer() {
+		return customer;
+	}
 
-    public static final class ItemRequest {
-        private final String product;
-        private final Integer quantity;
-        private final String detail;
-        private final Integer price;
+	public OrderRequest customer(final CustomerRequest customer) {
+		this.customer = customer;
 
-        public ItemRequest(String product, Integer quantity, String detail, Integer price) {
-            this.product = product;
-            this.quantity = quantity;
-            this.detail = detail;
-            this.price = price;
-        }
+		return this;
+	}
 
-        public String getProduct() {
-            return product;
-        }
+	@Override
+	public String toString() {
+		return new StringBuilder("OrderRequest{").append("ownId='").append(ownId).append('\'').append(", items=")
+				.append(items).append(", customer=").append(customer).append('}').toString();
+	}
 
-        public Integer getQuantity() {
-            return quantity;
-        }
+	public static final class ItemRequest {
+		private final String product;
+		private final Integer quantity;
+		private final String detail;
+		private final Integer price;
 
-        public String getDetail() {
-            return detail;
-        }
+		public ItemRequest(String product, Integer quantity, String detail, Integer price) {
+			this.product = product;
+			this.quantity = quantity;
+			this.detail = detail;
+			this.price = price;
+		}
 
-        public Integer getPrice() {
-            return price;
-        }
+		public String getProduct() {
+			return product;
+		}
 
-        @Override
-        public String toString() {
-            return new StringBuilder("Item{")
-                    .append("product='").append(product).append('\'')
-                    .append(", quantity=").append(quantity)
-                    .append(", detail='").append(detail).append('\'')
-                    .append(", price=").append(price)
-                    .append('}').toString();
-        }
-    }
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public String getDetail() {
+			return detail;
+		}
+
+		public Integer getPrice() {
+			return price;
+		}
+
+		@Override
+		public String toString() {
+			return new StringBuilder("Item{")
+					.append("product='").append(product).append('\'')
+					.append(", quantity=").append(quantity)
+					.append(", detail='").append(detail).append('\'')
+					.append(", price=").append(price)
+					.append('}').toString();
+		}
+	}
 
 }
