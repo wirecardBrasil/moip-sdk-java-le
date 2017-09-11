14de47140f7eb63de8b9c187003bf35aee0f1a0c
Caio Gaspar <caio.gaspar@moip.com.br>
2017-09-11 15:30:17 -0300

    refactor: Refactor GrantType enum
    Refactor GrantType enum

--- src/main/java/br/com/moip/request/GrantType.java
+++ src/main/java/br/com/moip/request/GrantType.java
@@ -1,5 +1,16 @@
 package br.com.moip.request;
 
 public enum GrantType {
-    authorization_code, refresh_token
+    AUTHORIZATION_CODE("authorization_code"),
+    REFRESH_TOKEN("refresh_token");
+
+    private String description;
+
+    private GrantType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
