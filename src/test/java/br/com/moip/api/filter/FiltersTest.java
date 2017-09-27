5349675bd6627d44a274584bf3043b7cb34331b1
Caio Gaspar <caio.gaspar@moip.com.br>
2017-09-27 14:21:21 -0300

    refactor: Applied sugested fixes from PR
    Applied sugested fixes from PR

--- src/test/java/br/com/moip/api/filter/FiltersTest.java
+++ src/test/java/br/com/moip/api/filter/FiltersTest.java
@@ -2,65 +2,53 @@ package br.com.moip.api.filter;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
 public class FiltersTest {
 
     @Test
-    public void testGreaterThanFilter()
-    {
+    public void testGreaterThanFilter() {
         Filters filter = new Filters();
         filter.greaterThan("createdAt", "2017-08-17");
         assertEquals("createdAt::gt(2017-08-17)", filter.toString());
     }
 
     @Test
-    public void testGreaterEqualFilter()
-    {
+    public void testGreaterEqualFilter() {
         Filters filter = new Filters();
         filter.greaterThanOrEqual("createdAt", "2017-08-17");
         assertEquals("createdAt::ge(2017-08-17)", filter.toString());
     }
 
     @Test
-    public void testLessThanFilter()
-    {
+    public void testLessThanFilter() {
         Filters filter = new Filters();
         filter.lessThan("value", "100000");
         assertEquals("value::lt(100000)", filter.toString());
     }
 
     @Test
-    public void testInFilter()
-    {
+    public void testInFilter() {
         Filters filter = new Filters();
-        ArrayList<String> values = new ArrayList<>();
-        values.add("BOLETO");
-        values.add("DEBIT_CARD");
-        values.add("ONLINE_BANK_DEBIT");
-        filter.in("paymentMethod", values);
+        filter.in("paymentMethod", Arrays.asList(new String[] {"BOLETO","DEBIT_CARD","ONLINE_BANK_DEBIT"}));
         assertEquals("paymentMethod::in(BOLETO,DEBIT_CARD,ONLINE_BANK_DEBIT)", filter.toString());
     }
 
     @Test
-    public void testBetweenFilter()
-    {
+    public void testBetweenFilter() {
         Filters filter = new Filters();
         filter.between("createdAt", "2017-08-10", "2017-08-17");
+
         assertEquals("createdAt::bt(2017-08-10,2017-08-17)", filter.toString());
     }
 
     @Test
-    public void testMultipleFilter()
-    {
+    public void testMultipleFilter() {
         Filters filter = new Filters();
-        ArrayList<String> values = new ArrayList<>();
-        values.add("BOLETO");
-        values.add("DEBIT_CARD");
-        filter.greaterThanOrEqual("createdAt", "2017-08-17")
-            .in("paymentMethod", values)
+        filter.greaterThanOrEqual("createdAt", "2017-08-17");
+        filter.in("paymentMethod", Arrays.asList(new String[] {"BOLETO","DEBIT_CARD"}))
             .lessThan("value", "100000");
         assertEquals("createdAt::ge(2017-08-17)|paymentMethod::in(BOLETO,DEBIT_CARD)|value::lt(100000)", filter.toString());
     }
