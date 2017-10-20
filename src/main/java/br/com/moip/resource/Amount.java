e90fce050f1b189c95cefcd963cb8a27a5c13c6a
Vitaly Kudrin <vkudrin@yandex.ru>
2017-10-20 23:48:37 +0300

    Added fees, refunds, liquid, currency properties and getters for them Fix #81

--- src/main/java/br/com/moip/resource/Amount.java
+++ src/main/java/br/com/moip/resource/Amount.java
@@ -3,9 +3,29 @@ package br.com.moip.resource;
 public class Amount {
 
     private Integer total;
+    private Integer fees;
+    private Integer refunds;
+    private Integer liquid;
+    private String currency;
 
     public Integer getTotal() {
         return total;
+    }
+
+    public Integer getFees() {
+        return fees;
+    }
+
+    public Integer getRefunds() {
+        return refunds;
+    }
+
+    public Integer getLiquid() {
+        return liquid;
+    }
+
+    public String getCurrency() {
+        return currency;
     }
 
     @Override
