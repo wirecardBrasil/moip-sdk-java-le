58a46081235a5ec24d85a544c3af74dee284f2e8
Matheus Nakaya <mbnakaya25@gmail.com>
2017-11-21 10:16:32 -0200

    refactor(account): refactor AccountAPITest import

--- src/test/java/br/com/moip/api/AccountAPITest.java
+++ src/test/java/br/com/moip/api/AccountAPITest.java
@@ -17,7 +17,11 @@ import org.junit.Test;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 
 public class AccountAPITest {
 
