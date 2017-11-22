6039fb804c5c9b462b0b2ca73c69d66c17fcd98f
Matheus Nakaya <mbnakaya25@gmail.com>
2017-11-22 09:57:38 -0200

    refactor(account): adjusted AccountLinks and .travis.yml

--- src/main/java/br/com/moip/resource/AccountLinks.java
+++ src/main/java/br/com/moip/resource/AccountLinks.java
@@ -4,49 +4,17 @@ import br.com.moip.resource.links.Href;
 
 public class AccountLinks {
 
-    private Self self;
-    private Password setPassword;
+    private Href self;
+    private Href setPassword;
 
-    public Self getSelf() { return self; }
+    public Href getSelf() { return self; }
 
-    public void setSelf(Self self) { this.self = self; }
+    public void setSelf(Href self) { this.self = self; }
 
-    public Password getSetPassword() { return setPassword; }
+    public Href getSetPassword() { return setPassword; }
 
-    public void setSetPassword(Password setPassword) { this.setPassword = setPassword; }
+    public void setSetPassword(Href setPassword) { this.setPassword = setPassword; }
 
-    public class Self{
-
-        private String href;
-
-        public String getHref() { return href; }
-
-        public void setHref(String href) { this.href = href; }
-
-        @Override
-        public String toString() {
-            final StringBuilder sb = new StringBuilder("Self{");
-            sb.append("href=").append(href);
-            sb.append('}');
-            return sb.toString();
-        }
-    }
-    public class Password{
-
-        private String href;
-
-        public String getHref() { return href; }
-
-        public void setHref(String href) { this.href = href; }
-
-        @Override
-        public String toString() {
-            final StringBuilder sb = new StringBuilder("SetPassword{");
-            sb.append("href=").append(href);
-            sb.append('}');
-            return sb.toString();
-        }
-    }
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Links{");
