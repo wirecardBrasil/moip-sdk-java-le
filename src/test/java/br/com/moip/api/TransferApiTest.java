568d3e267b953ce11fe4a0cd754c72c490768aee
Matheus Nakaya <mbnakaya25@gmail.com>
2017-11-29 14:38:34 -0200

    style: specify the imports are being used

--- src/test/java/br/com/moip/api/TransferApiTest.java
+++ src/test/java/br/com/moip/api/TransferApiTest.java
@@ -2,7 +2,10 @@ package br.com.moip.api;
 
 import br.com.moip.Client;
 import br.com.moip.request.TransferRequest;
-import br.com.moip.resource.*;
+
+import br.com.moip.resource.Transfer;
+import br.com.moip.resource.TransferInstrument;
+import br.com.moip.resource.TransferStatus;
 import br.com.moip.response.TransferListResponse;
 import com.rodrigosaito.mockwebserver.player.Play;
 import com.rodrigosaito.mockwebserver.player.Player;
