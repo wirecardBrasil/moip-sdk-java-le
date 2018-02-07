4a0939d66ad2b3ff90441a4e1a07e986f3501e3f
Matheus Nakaya <mbnakaya25@gmail.com>
2018-02-07 09:31:50 -0200

    refactor(EntriesListResponse): delete unused methods and elements

--- src/main/java/br/com/moip/response/EntriesListResponse.java
+++ src/main/java/br/com/moip/response/EntriesListResponse.java
@@ -9,30 +9,12 @@ import java.util.List;
 
 public class EntriesListResponse extends ArrayList<Entry> {
 
-    public static final String NEXT = "next";
-    public static final String PREVIOUS = "previous";
-
     private List<Entry> entries;
-    private Summary summary;
-    private Links links;
-
-    public String next() { return links.getLinks().get(NEXT).toString(); }
-
-    public String previous() { return links.getLinks().get(PREVIOUS).toString(); }
-
-    public List<Entry> getEntries() { return entries; }
-
-    public void setEntries(List<Entry> entries) { this.entries = entries; }
-
-    public Summary getSummary() { return summary; }
-
-    public void setSummary(Summary summary) { this.summary = summary; }
 
     @Override
     public String toString() {
-        return new StringBuilder("EntriesListResponse{")
-                .append("summary=").append(summary)
-                .append(", entries=").append(entries)
-                .append('}').toString();
+        return new StringBuilder("EntriesListResponse=[")
+                .append(entries)
+                .append(']').toString();
     }
 }
