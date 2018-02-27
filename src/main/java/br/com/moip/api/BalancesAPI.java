4aa8c6d906be6e9ac613660dfb42645c0177f261
Matheus Nakaya <mbnakaya25@gmail.com>
2018-02-27 19:26:16 -0300

    refactor BalancesAPI

--- src/main/java/br/com/moip/api/BalancesAPI.java
+++ src/main/java/br/com/moip/api/BalancesAPI.java
@@ -1,5 +1,6 @@
 package br.com.moip.api;
 
+import br.com.moip.API;
 import br.com.moip.Client;
 import br.com.moip.resource.Balances;
 
@@ -7,7 +8,11 @@ public class BalancesAPI {
 
     private final Client client;
 
+    private final String BALANCE_ENDPOINT = "/v2/balances";
+
+    private final String API_VERSION = "2.1";
+
     public BalancesAPI(final Client client) { this.client = client; }
 
-    public Balances get() { return client.get("/v2/balances", Balances.class, "2.1"); }
+    public Balances get() { return client.get(BALANCE_ENDPOINT, Balances.class, API_VERSION); }
 }
